c8b5db3dc3b5caa28b05222bbd30114a7824481b
Snail Mail <>
2018-02-01 21:02:45 -0800

    Support deployments without checking in frontend/dist/
    Create a separate dummy branch, and then deploy from that

--- deploy-heroku.sh
+++ deploy-heroku.sh
@@ -5,9 +5,18 @@ cd /project/frontend
 npm run build
 
 cd /project
+
+git checkout -b release
+git add -f frontend/dist/
+git config user.name 'Heroku'
+git config user.email '<>'
+git commit -m "AUTOMATED PUSH TO HEROKU"
+
 # Update Heroku's configuration to include the latest variables from our prod
 # environment configuration.
 sed 's/#[^("|'')]*$//;s/^#.*$//' prod.env | xargs heroku config:set
 
 # Deploy to Heroku.
-git push heroku master
+git push heroku release:master
+git checkout master
+git branch -D release
