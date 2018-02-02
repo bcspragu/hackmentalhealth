37395c1111038eb39717a494eb00e626eba5469c
Brandon Sprague <brandon00sprague@gmail.com>
2018-02-01 20:43:17 -0800

    Use production config settings for our deployment
    Also, tweak the deploy script to make more sense.

--- deploy-heroku.sh
+++ deploy-heroku.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+
+# Go into the frontend directory and build a compiled version of our site.
 cd /project/frontend
-npm run ng build
+npm run build
+
 cd /project
+# Update Heroku's configuration to include the latest variables from our prod
+# environment configuration.
 sed 's/#[^("|'')]*$//;s/^#.*$//' prod.env | xargs heroku config:set
+
+# Deploy to Heroku.
 git push heroku master
